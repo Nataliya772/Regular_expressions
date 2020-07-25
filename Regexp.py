213d710ec47c4f9adf1d3f52e9b210c35a2efb65
Nataliya772 <nsaxen@yandex.ru>
2020-07-25 18:31:33 +0300

    second

--- Regexp.py
+++ Regexp.py
@@ -1,4 +1,3 @@
-import itertools
 import csv
 import re
 # читаем адресную книгу в формате CSV в список contacts_list
@@ -47,66 +46,21 @@ def clean_text(contacts_list):
         pattern = re.compile(r"('Мартиняхин', 'Виталий', 'Геннадьевич',  'ФНС',)(\s)('',)")
         text_eleven = pattern.sub(r"\1\2'cоветник отдела Интернет проектов Управления информационных технологий',", str(text_ten))
         #print(text_eleven)
-        pattern = re.compile(r'(.*)')
-        text_new = (re.findall(pattern, str(text_eleven)))
-        phonebook.append(text_new)
+        pattern = re.compile(r',')
+        text_twelve = re.split(pattern, text_eleven)
+        #print(len(text_twelve))
+        if len(text_twelve) == 7:
+            pattern = re.compile(r'(.*)')
+            text_new = re.findall(pattern, str(text_eleven))
+            phonebook.append(text_new)
 
     print(phonebook)
 
 clean_text(contacts_list)
 
-
-
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
-#phonebook = []
-
-def get_name(contacts_list):
-    for contact in contacts_list:
-        pattern = re.compile(r"([А-Я]{1}[а-яё]+)")
-        text_new = (re.findall(pattern, str(contact)))
-        if text_new != []:
-            phonebook.append(text_new[0:3])
-
-#get_name(contacts_list)
-#print(phonebook)
-
-def get_sort_phonebook(phonebook):
-    phonebook_sort = sorted(phonebook)
-    phonebook_new = (list(phonebook_sort for phonebook_sort,_ in itertools.groupby(phonebook_sort)))
-    print(phonebook_new)
-    return phonebook_new
-
-get_sort_phonebook(phonebook)
-#print(phonebook_new)
-
-def get_organization():
-    for contact in contacts_list:
-        pattern = re.compile(r"([А-Я]{1}[а-яё]+)")
-        text_new = (re.findall(pattern, str(contact)))
-        contact_new = contact[0].split()
-        #print(contact_new)
-        phonebook_new = get_sort_phonebook(phonebook)
-        for value in phonebook_new:
-            if value[0:2] == value:
-                phonebook_new.remove(value)
-            if contact_new[0] in value:
-                value.append(contact[3])
-                value.append(contact[4])
-                value.append(contact[5])
-                value.append(contact[6])
-
-    print(phonebook_new)
-
-
-#del_duplicates()
-#get_organization()
-
-
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
 with open("phonebook.csv", "w", encoding='utf-8') as f:
     datawriter = csv.writer(f, delimiter=',')
-    phonebook_new = get_sort_phonebook(phonebook)
-    datawriter.writerows(phonebook_new)
+    datawriter.writerows(phonebook)
 
